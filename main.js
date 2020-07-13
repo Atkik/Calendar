597e655955433c7aa4c5a3185bba69f51090b9c8
Atkik <totomaru5525@i.softbank.jp>
2020-07-13 19:39:26 +0900

    output date

--- main.js
+++ main.js
@@ -6,13 +6,31 @@ function viewMonth(year, month) {
 
 //カレンダーテーブル表示関数
 function viewTable(year, month) {
+	//月初
 	var begginingMonth = new Date(year, month-1, 1);
+	//月初曜日
+	var begginingWeekDay = begginingMonth.getDay();
+	//月末
+	var endMonth = new Date(year, month, 0);
+	var endMonthDate = endMonth.getDate();
 
 	var monthTable = document.getElementsByClassName("calendar-table");
+	
+	var monthStartFlug = 0;
+	var dateCount = 1;
+	
 	for(var i = 0; i < 5; i++) {
 		var weekRow = monthTable[0].insertRow(-1);
 		for(var j = 0; j < 7; j++) {
 			var dayCell = weekRow.insertCell(-1);
+			//月初の曜日から月末まで日付を表示する
+			if(begginingWeekDay > monthStartFlug) {
+				monthStartFlug++;
+			} else if(dateCount != (endMonthDate + 1)) {
+				dayCell.innerHTML = "<span>" + dateCount + "</span>";
+				dateCount++;
+			}
+			
 		}
 	}
 }
