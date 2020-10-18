dee777f59d3d92f2ee0627e569965fc7fe3783d7
Atkik <totomaru5525@i.softbank.jp>
2020-10-18 16:20:11 +0900

    create table

--- index.php
+++ index.php
@@ -1,17 +1,34 @@
 <?php
-	include_once("index.html");
+//データベース接続用の情報を代入
+$dbname='heroku_eecf5ac830e1d0f';
+$dsn = 'mysql:dbname='.$dbname.';host=us-cdbr-east-02.cleardb.com';
+$user = 'b94fd4abf6b46b';
+$password = 'b0e7c029';
 
-    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+try{
+    $dbh = new PDO($dsn, $user, $password);
+}catch (PDOException $e){
+    print('Error:'.$e->getMessage());
+    die();
+}
 
-    $server = $url["us-cdbr-east-02.cleardb.com"];
-    $username = $url["b94fd4abf6b46b"];
-    $password = $url["b0e7c029"];
-    $db = substr($url["heroku_eecf5ac830e1d0f"], 1);
 
-    $link = mysqli_connect($server, $username, $password, $db);
-    $result = mysqli_query($link, "select * from test_table");
+//scheduleテーブルがない場合にテーブルを作成するsql
+$sql ='CREATE TABLE '.$dbname.'.schedule (
+	No INT AUTO_INCREMENT,
+	date DATETIME NOT NULL,
+	schedule VARCHAR(300),
+	PRIMARY KEY (No)
+) ENGINE = InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci';
 
-    while($user = mysqli_fetch_array($result)) {
-      echo $user['key_no'], " : ", $user['text'], "<br>";
-    }
+
+//sqlを実行
+$stmt = $dbh->query($sql);
+//$result = $stmt->fetchall();
+
+//index.htmlを読み込む
+include_once("index.html");
+
+$dbh = null;
+
 ?>
